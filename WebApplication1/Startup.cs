53d995e90f06e7135a20e818e907ce409fd94309
Sravan <sravankumar8730@gmail.com>
2021-11-16 16:05:27 +0530

    Apples Added

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -33,6 +33,7 @@ namespace WebApplication1
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("<h1>Hello World!<h1>");
+                    await context.Response.WriteAsync("<h1>Hello World!<h1> 12345");
                 });
             });
         }
